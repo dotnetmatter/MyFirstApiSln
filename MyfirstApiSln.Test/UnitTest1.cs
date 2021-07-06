1c3a1d570ee1fdefa4cc83ea7d00763feefa33e8
dotnetmatter <lalanthir@gmail.com>
2021-07-06 18:22:05 +1000

    add init

---
+++ MyfirstApiSln.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace MyfirstApiSln.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(5,5);
+
+        }
+    }
+}
